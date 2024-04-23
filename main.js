b94679aba5ab05f8e834449f3b588118f66a9814
CeresaClark <157949231+CeresaClark@users.noreply.github.com>
2024-04-23 08:53:06 +0000

    v1.1

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 var video = document.getElementById("video")
 $(".button").on("click",function(){
-    $("#video").fadeIn(500)
+    $(".bg").addClass("bg2")
+    $("#video").delay(500).fadeIn(500)
     $(".button").fadeOut(100)
     video.play()
     $(".white").delay(4000).fadeIn(100)
-    $(".white").delay(200).fadeOut(1000)
-    $("#video").delay(4100).fadeOut(200)
-    $(".black").delay(4500).fadeIn(500)
+    $(".white").delay(100).fadeOut(1000)
+    $("#video").delay(3500).fadeOut(200)
+    $(".black").delay(4200).fadeIn(500)
     $(".re").fadeIn(500)
     $(".re").delay(4500).animate({
         top: "150px"
@@ -16,6 +17,7 @@ $(".button").on("click",function(){
 
 $(".black").on("click",function(){
     $(".re").fadeOut(500)
+    $(".bg").removeClass("bg2")
     $(".re").css("top","-700px")
     $(".black").fadeOut(500)
     $(".button").fadeIn(200)
